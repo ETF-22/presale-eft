e10e438feb7c56be9816c284b43e236ffbf00423
ETF-22 <m7660166m766@gmail.com>
2025-06-23 14:39:02 +0400

    Final full-featured presale version

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white bg-black">
+      <h1 className="text-4xl font-bold">EFT Presale is Live!</h1>
+    </div>
+  );
+};
+
+export default App;
